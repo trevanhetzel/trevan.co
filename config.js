76bfa1695d6f8470f022eef011eb307de4bf93dd
Trevan Hetzel <trevan@hetzelcreative.com>
2014-04-30 12:43:17 -0500

    Update footer picture

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ config = {
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        url: 'http://my-ghost-blog.com',
+        url: 'http://trevan.co',
 
         // Example mail config
         // Visit http://docs.ghost.org/mail for instructions
@@ -44,7 +44,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://trevan.co',
         mail: {},
         database: {
             client: 'sqlite3',
@@ -57,7 +57,7 @@ config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
+            port: '10433'
         }
     },
 
