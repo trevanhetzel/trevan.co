252be1287316e2cefb35d8fae7832e3adf14cc59
Trevan Hetzel <trevan@hetzelcreative.com>
2015-09-26 22:56:37 -0500

    Add footer

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
   </div>
 
+  <footer class="footer">
+    <p class="footer__p">&copy; 2015 Trevan Hetzel&nbsp;&nbsp;|&nbsp;&nbsp;Built with <a href="#">WordPress, Twig &amp; Backbone</a> on <a href="http://getflywheel.com">Flywheel</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">Source code</a></p>
+  </footer>
+
   <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/bundle.js"></script>
   
   <?php wp_footer(); ?>
