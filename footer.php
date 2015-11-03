2778193cc909f6f175d505ee29ed18028f6ce9cd
Trevan Hetzel <trevan@hetzelcreative.com>
2015-11-02 19:48:05 -0600

    Add async attr to script because pagespeed insights is dumb

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
     <p class="footer__p">&copy; <?php the_time('Y'); ?> Trevan Hetzel&nbsp;&nbsp;|&nbsp;&nbsp;Built with <a href="#">WordPress, Twig &amp; Backbone</a> on <a href="http://getflywheel.com">Flywheel</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">Source code</a></p>
   </footer>
 
-  <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/bundle.js"></script>
+  <script async src="<?php echo esc_url( get_template_directory_uri() ); ?>/bundle.js"></script>
   
   <?php wp_footer(); ?>
 
