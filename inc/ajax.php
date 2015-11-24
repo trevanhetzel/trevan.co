b45821a59b17b61ff7d0abd6517df20d8fc03591
Trevan Hetzel <trevan@hetzelcreative.com>
2015-11-23 22:32:51 -0600

    Return ID with ajax

--- inc/ajax.php
+++ inc/ajax.php
@@ -97,7 +97,8 @@ function return_one () {
         'day' => get_the_date('j'),
         'month' => get_the_date('M'),
         'content' => apply_filters('the_content', $post->post_content),
-        'categories' => $cats
+        'categories' => $cats,
+        'id' => $post->ID
       );
     }
   }
